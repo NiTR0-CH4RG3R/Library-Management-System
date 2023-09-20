3096670fc16d13a0c6e7ba0ca8cafcf8d8d6dc8f
SMadu99 <gasmshanilka@gmail.com>
2023-09-20 20:21:28 +0530

    Added  BorrowRequest Form for Client Application

---
+++ lms-client/View/BorrowRequestForm.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace lms_client.View
+{
+    partial class BorrowRequestForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            listView1 = new ListView();
+            buttonRemove = new Button();
+            SuspendLayout();
+            // 
+            // listView1
+            // 
+            listView1.Location = new Point(42, 33);
+            listView1.Name = "listView1";
+            listView1.Size = new Size(726, 290);
+            listView1.TabIndex = 0;
+            listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // buttonRemove
+            // 
+            buttonRemove.Location = new Point(612, 370);
+            buttonRemove.Name = "buttonRemove";
+            buttonRemove.Size = new Size(156, 38);
+            buttonRemove.TabIndex = 1;
+            buttonRemove.Text = "Remove";
+            buttonRemove.UseVisualStyleBackColor = true;
+            // 
+            // BorrowRequestForm
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(buttonRemove);
+            Controls.Add(listView1);
+            Name = "BorrowRequestForm";
+            Text = "BorrowRequestForm";
+            Load += BorrowRequestForm_Load;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ListView listView1;
+        private Button buttonRemove;
+    }
+}
---
+++ lms-client/View/BorrowRequestForm.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lms_client.View
+{
+    public partial class BorrowRequestForm : Form
+    {
+        public BorrowRequestForm()
+        {
+            InitializeComponent();
+        }
+
+        private void BorrowRequestForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
