09aab0e5563945be736c2378c3d1c5c178295d1b
Buddhima Zoysa <buddhimakaveeshwara@gmail.com>
2023-09-20 23:33:42 +0530

    Added a testing project.

---
+++ Testing/LOL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.Pkcs;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testing
+{
+    internal class LOL
+    {
+        private static LOL? singleton = null;
+
+        public static LOL GetSingleton()
+        {
+            if (singleton == null) singleton = new LOL();
+            return singleton;
+        }
+
+        public static LOL Singleton
+        {
+            get
+            {
+                if (singleton == null) singleton = new LOL();
+                return singleton;
+            }
+        }
+
+        private LOL()
+        {
+
+        }
+    }
+}
---
+++ Testing/Program.cs
@@ -0,0 +1,22 @@
+﻿//using Microsoft.VisualBasic.Logging;
+//using System.Reflection;
+//using Testing;
+
+//lms_common.Database db = new lms_common.Database("", "");
+
+//lms_common.Model.Book b = new lms_common.Model.Book();
+//b.ISBN = "0123456789012";
+//b.Qty = 2;
+
+//db.UpdateByPrimaryKey(b);
+
+
+
+
+////Testing.LOL l = new Testing.LOL();
+
+////var a = typeof(LOL).GetFields()[0].GetValue(l);
+////Console.WriteLine(a);
+
+Console.WriteLine(typeof(bool).Name);
+Console.ReadLine();
